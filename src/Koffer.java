6b4a1891c2853f05f52673de33e3a74c3168d16b
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-06-15 14:35:43 +0200

    Erste Version des Koffers (Auspacken noch nicht möglich) implementiert!

--- src/Koffer.java
+++ src/Koffer.java
@@ -1,3 +1,29 @@
 public class Koffer {
+    // Attribute
+    public int kapazität;
+    public String inhalt;
 
+    public Koffer(int kapazität, String inhalt) {
+        this.kapazität = kapazität;
+        this.inhalt = inhalt;
+    }
+
+    public void einpacken(String gegenstand) {
+        if (inhalt.length() + gegenstand.length() > kapazität) {
+            System.out.println("Koffer ist schon voll");
+            return;
+        }
+        if ( ! inhalt.isEmpty() ) {
+            inhalt += ", ";
+        }
+        inhalt += gegenstand;
+    }
+
+    public void auspacken(String gegenstand) {
+        // TODO
+    }
+
+    public void inhaltAnzeigen() {
+        System.out.println("Der Koffer enthält: " + inhalt);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Koffer koffer = new Koffer(120, "");
 
+        koffer.einpacken("Hose");
+        koffer.einpacken("Regenschirm");
+        koffer.einpacken("Sonnenbrille");
+        koffer.einpacken("Tablet");
+        koffer.einpacken("Urlaubsgenehmigungszustimmungsformular in zweifacher Ausfertigung mit Gültigkeitsnachweiszertifikat");
+
+        koffer.inhaltAnzeigen();
     }
 }
